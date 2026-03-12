524f94a0dfc720fa3bd3ad2e27bde7e503ae2f19
aquadet-sketch <aquadet@gmail.com>
2026-03-13 00:20:07 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,35 +1,49 @@
-const list = document.getElementById("product-list")
+const grid = document.getElementById("product-grid");
 
-if(list){
+if(grid){
 
-products.forEach(p=>{
+products.forEach(product => {
 
-const card = document.createElement("div")
+const card = document.createElement("div");
 
-card.className="card"
+card.className = "card";
 
 card.innerHTML = `
-<h3>${p.name}</h3>
-<p>${p.desc}</p>
-<a href="product.html?id=${p.id}">View Details</a>
-`
+<h3>${product.name}</h3>
+<p>${product.description}</p>
+<a class="btn-small" href="product.html?id=${product.id}">View Details</a>
+`;
 
-list.appendChild(card)
+grid.appendChild(card);
 
-})
+});
+
 }
 
-const params = new URLSearchParams(window.location.search)
-const id = params.get("id")
+
+const params = new URLSearchParams(window.location.search);
+const id = params.get("id");
 
 if(id){
 
-const product = products.find(p=>p.id===id)
+const product = products.find(p => p.id === id);
 
 if(product){
 
-document.getElementById("product-title").innerText = product.name
-document.getElementById("product-desc").innerText = product.desc
+document.getElementById("title").innerText = product.name;
+document.getElementById("description").innerText = product.description;
+
+const list = document.getElementById("benefits");
+
+product.benefits.forEach(b => {
+
+const li = document.createElement("li");
+li.innerText = b;
+list.appendChild(li);
+
+});
+
+document.getElementById("application").innerText = product.application;
 
 }
 
